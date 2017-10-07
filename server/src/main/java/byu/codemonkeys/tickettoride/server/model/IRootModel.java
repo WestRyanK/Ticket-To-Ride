0d42ea37733d2f0ef22902d788c7afbd02079cbe
Jacob Easley <jeasley@localhost.localdomain>
2017-10-07 11:21:45 -0600

    Write ServerFacade tests

---
+++ server/src/main/java/byu/codemonkeys/tickettoride/server/model/IRootModel.java
@@ -0,0 +1,32 @@
+package byu.codemonkeys.tickettoride.server.model;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public interface IRootModel {
+    public boolean verifyLogin(String username, String password);
+
+    public boolean authorize(String authToken);
+
+    public void removeSession(String authToken);
+
+    public User getUser(String username);
+
+    public void registerNewUser(String username, String password);
+
+    public String generateUniqueID();
+
+    public void addSession(ServerSession session);
+
+    public ServerSession getSession(String authToken);
+
+    public List<PendingGame> getPendingGames();
+
+    public PendingGame getPendingGame(String gameID);
+
+    public void addPendingGame(PendingGame game);
+
+    public void removePendingGame(String gameID);
+
+    public ActiveGame activateGame(String gameID) throws NoSuchElementException;
+}
---
+++ server/src/test/java/byu/codemonkeys/tickettoride/server/ServerFacadeTest.java
@@ -0,0 +1,83 @@
+package byu.codemonkeys.tickettoride.server;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import byu.codemonkeys.tickettoride.server.model.MockRootModel;
+
+import static org.junit.Assert.*;
+
+public class ServerFacadeTest {
+    @Before
+    public void setUp() {
+        ServerFacade.initialize(new MockRootModel());
+    }
+
+    @Test
+    public void testGetInstance() {
+        assertNotNull(ServerFacade.getInstance());
+    }
+
+    @Test
+    public void testLogin() {
+        assertFalse(ServerFacade.getInstance().login("user", "password").isSuccessful());
+        assertFalse(ServerFacade.getInstance().login("username", "secret").isSuccessful());
+        assertTrue(ServerFacade.getInstance().login("username", "password").isSuccessful());
+    }
+
+    @Test
+    public void testRegister() {
+        assertFalse(ServerFacade.getInstance().register("username", "secret").isSuccessful());
+        assertTrue(ServerFacade.getInstance().register("user", "password").isSuccessful());
+    }
+
+    @Test
+    public void testLogout() {
+        assertTrue(ServerFacade.getInstance().logout("any-auth-token").isSuccessful());
+    }
+
+    @Test
+    public void testCreateGame() {
+        assertFalse(ServerFacade.getInstance().createGame("invalid-auth-token", "game-name").isSuccessful());
+        assertTrue(ServerFacade.getInstance().createGame("auth-token", "game-name").isSuccessful());
+    }
+
+    @Test
+    public void joinPendingGame() {
+        assertFalse(ServerFacade.getInstance().joinPendingGame("invalid-auth-token", "game-1-id").isSuccessful());
+        assertFalse(ServerFacade.getInstance().joinPendingGame("auth-token", "invalid-game-id").isSuccessful());
+        assertFalse(ServerFacade.getInstance().joinPendingGame("auth-token", "game-1-id").isSuccessful());
+        assertFalse(ServerFacade.getInstance().joinPendingGame("auth-token", "full-game-id").isSuccessful());
+        assertTrue(ServerFacade.getInstance().joinPendingGame("auth-token", "game-2-id").isSuccessful());
+    }
+
+    @Test
+    public void testLeavePendingGame() {
+        assertFalse(ServerFacade.getInstance().leavePendingGame("invalid-auth-token", "game-1-id").isSuccessful());
+        assertFalse(ServerFacade.getInstance().leavePendingGame("auth-token","invalid-game-id").isSuccessful());
+        assertTrue(ServerFacade.getInstance().leavePendingGame("auth-token", "game-1-id").isSuccessful());
+    }
+
+    @Test
+    public void testCancelGame() {
+        assertFalse(ServerFacade.getInstance().cancelGame("invalid-auth-token", "game-1-id").isSuccessful());
+        assertFalse(ServerFacade.getInstance().cancelGame("auth-token", "invalid-game-id").isSuccessful());
+        assertFalse(ServerFacade.getInstance().cancelGame("auth-token", "game-2-id").isSuccessful());
+        assertTrue(ServerFacade.getInstance().cancelGame("auth-token", "game-1-id").isSuccessful());
+    }
+
+    @Test
+    public void testGetPendingGames() {
+        assertFalse(ServerFacade.getInstance().getPendingGames("invalid-auth-token").isSuccessful());
+        assertTrue(ServerFacade.getInstance().getPendingGames("auth-token").isSuccessful());
+    }
+
+    @Test
+    public void testStartGame() {
+        assertFalse(ServerFacade.getInstance().startGame("invalid-auth-token", "game-1-id").isSuccessful());
+        assertFalse(ServerFacade.getInstance().startGame("auth-token", "invalid-game-id").isSuccessful());
+        assertFalse(ServerFacade.getInstance().startGame("auth-token", "full-game-id").isSuccessful());
+        assertFalse(ServerFacade.getInstance().startGame("auth-token", "game-1-id").isSuccessful());
+        assertTrue(ServerFacade.getInstance().startGame("auth-token-0", "full-game-id").isSuccessful());
+    }
+}
