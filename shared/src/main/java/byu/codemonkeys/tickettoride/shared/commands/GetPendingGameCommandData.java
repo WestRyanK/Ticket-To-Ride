e08a429707d906fed62428ddb7a676846711c9e6
David Corey <dcorey@qualtrics.com>
2017-10-07 19:37:51 -0600

    Added getPendingGameCommand

--- shared/src/main/java/byu/codemonkeys/tickettoride/shared/commands/GetPendingGameCommandData.java
+++ shared/src/main/java/byu/codemonkeys/tickettoride/shared/commands/GetPendingGameCommandData.java
@@ -1,7 +1,15 @@
 package byu.codemonkeys.tickettoride.shared.commands;
 
 public class GetPendingGameCommandData extends CommandData {
-	public GetPendingGameCommandData() {
-		super(CommandType.GET_PENDING_GAMES);
+	protected String gameID;
+	public GetPendingGameCommandData(String gameID) {
+		super(CommandType.GET_PENDING_GAME);
+		this.gameID = gameID;
+	}
+
+
+	public String getGameID() {
+		return this.gameID;
 	}
 }
+
--- shared/src/main/java/byu/codemonkeys/tickettoride/shared/commands/GetPendingGamesCommandData.java
+++ shared/src/main/java/byu/codemonkeys/tickettoride/shared/commands/GetPendingGamesCommandData.java
@@ -1,6 +1,7 @@
 package byu.codemonkeys.tickettoride.shared.commands;
 
 public class GetPendingGamesCommandData extends CommandData {
+	protected String gameID;
 	public GetPendingGamesCommandData() {
 		super(CommandType.GET_PENDING_GAMES);
 	}
