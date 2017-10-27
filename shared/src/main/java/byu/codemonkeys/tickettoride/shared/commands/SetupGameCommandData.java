f40967b2a25a57442dfc7418837fe127f0f15a00
Jacob Easley <jacobbrianeasley@outlook.com>
2017-10-26 18:06:53 -0600

    Use Collection interfaces rather than concrete classes and references rather than shallow copies

--- shared/src/main/java/byu/codemonkeys/tickettoride/shared/commands/SetupGameCommandData.java
+++ shared/src/main/java/byu/codemonkeys/tickettoride/shared/commands/SetupGameCommandData.java
@@ -4,25 +4,25 @@ import byu.codemonkeys.tickettoride.shared.model.Color;
 import byu.codemonkeys.tickettoride.shared.model.UserBase;
 import byu.codemonkeys.tickettoride.shared.model.TrainCard;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 /**
  * Created by meganrich on 10/18/17.
  */
 
 public class SetupGameCommandData extends CommandData {
-    protected SetupGameCommandData(ArrayList<UserBase> order, Map<Color, UserBase> colors,
-                                   int numTrains, ArrayList<TrainCard> trainCards) {
+    protected SetupGameCommandData(List<UserBase> order, Map<Color, UserBase> colors,
+                                   int numTrains, List<TrainCard> trainCards) {
         super(CommandType.SETUP_GAME);
-        for(UserBase user: order) this.order.add(user);
-        for(TrainCard card: trainCards) this.trainCards.add(card);
-        this.colors.putAll(colors);
+        this.order = order;
+        this.trainCards = trainCards;
+        this.colors = colors;
         this.numTrains = numTrains;
-
     }
 
-    protected ArrayList<UserBase> order;
+    protected List<UserBase> order;
     protected Map<Color, UserBase> colors;
     protected int numTrains;
-    protected ArrayList<TrainCard> trainCards;
+    protected List<TrainCard> trainCards;
 
 }
