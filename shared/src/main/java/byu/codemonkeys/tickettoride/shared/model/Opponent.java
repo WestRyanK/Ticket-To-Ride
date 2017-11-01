8a67f5bcab3688f0b7f726d179f4be98aab43e0d
westryank1 <west.ryan.k@gmail.com>
2017-11-01 12:49:14 -0600

    Begin game sets the destination cards

--- shared/src/main/java/byu/codemonkeys/tickettoride/shared/model/Opponent.java
+++ shared/src/main/java/byu/codemonkeys/tickettoride/shared/model/Opponent.java
@@ -11,4 +11,10 @@ public class Opponent extends Player {
         this.numDestinationCards = player.getNumDestinationCards();
         this.color = player.getColor();
     }
+    
+    public void setNumDestinationCards(int numDestinationCards){
+        this.numDestinationCards = numDestinationCards;
+        setChanged();
+        notifyObservers();
+    }
 }
