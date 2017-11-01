0c3435f184c614ebce851f4428dfe07a4f0e4808
westryank1 <west.ryan.k@gmail.com>
2017-10-31 21:15:33 -0600

    Fixed broken test. The json had train names whereas we are now using colors

--- app/src/main/java/byu/codemonkeys/tickettoride/utils/HistoryDeserializer.java
+++ app/src/main/java/byu/codemonkeys/tickettoride/utils/HistoryDeserializer.java
@@ -3,6 +3,7 @@ package byu.codemonkeys.tickettoride.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
@@ -62,6 +63,7 @@ public class HistoryDeserializer {
             return new HistoryResult(obj.get("errorMessage").getAsString());
         }
         if (obj.has("history")) {
+            JsonElement element = obj.get("history");
             history = HistoryDeserializer.deserialize(obj.get("history").toString());
         }
 
