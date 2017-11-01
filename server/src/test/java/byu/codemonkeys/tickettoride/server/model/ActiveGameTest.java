dd50b62441ad4a46cfeb9a41b7a7e892fdd5fb5d
Jacob Easley <jacobbrianeasley@outlook.com>
2017-10-31 23:11:12 -0600

    Add some tests

---
+++ server/src/test/java/byu/codemonkeys/tickettoride/server/model/ActiveGameTest.java
@@ -0,0 +1,62 @@
+package byu.codemonkeys.tickettoride.server.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import byu.codemonkeys.tickettoride.shared.model.Player;
+import byu.codemonkeys.tickettoride.shared.model.PlayerColor;
+import byu.codemonkeys.tickettoride.shared.model.Self;
+import byu.codemonkeys.tickettoride.shared.model.cards.DestinationCard;
+
+import static org.junit.Assert.*;
+
+public class ActiveGameTest {
+    private ActiveGame game;
+
+    @Before
+    public void setUp() {
+        User owner = new User("Owner", "password");
+        User opponent = new User("Opponent", "supersecure");
+
+        PendingGame pendingGame = new PendingGame("My Game ID", "My Game", owner);
+        pendingGame.addUser(opponent);
+
+        game = new ActiveGame(pendingGame);
+    }
+
+    @Test
+    public void deal() throws Exception {
+        for (Player player : game.getPlayers()) {
+            Self self = (Self) player;
+
+            assertEquals(3, self.getSelecting().size());
+            assertEquals(4, self.getNumTrainCards());
+        }
+    }
+
+    @Test
+    public void prepareForClientTest() {
+        byu.codemonkeys.tickettoride.shared.model.ActiveGame preparedGame = game.prepareForClient(game.getOwner());
+
+        Self self = preparedGame.getSelf();
+
+        assertEquals(game.getOwner().getUsername(), self.getUsername());
+        assertEquals(0, self.getNumDestinationCards());
+        assertEquals(3, self.getSelecting().size());
+
+        for (DestinationCard card : self.getSelecting()) {
+            assertNotNull(card);
+        }
+
+        assertEquals(4, self.getNumTrainCards());
+
+        List<PlayerColor> used = new ArrayList<>();
+        for (Player player : preparedGame.getPlayers()) {
+            assertFalse(used.contains(player.getColor()));
+            used.add(player.getColor());
+        }
+    }
+}
