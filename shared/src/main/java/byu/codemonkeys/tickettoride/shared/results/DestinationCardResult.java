7d3203abd6b371683de7304d495f4ea823ecf2c1
Jacob Easley <jacobbrianeasley@outlook.com>
2017-11-10 16:11:57 -0700

    Document IServer / Allow DestinationCardResult to be constructed with a Set because a player's destination cards are stored as a Set on the server

--- shared/src/main/java/byu/codemonkeys/tickettoride/shared/results/DestinationCardResult.java
+++ shared/src/main/java/byu/codemonkeys/tickettoride/shared/results/DestinationCardResult.java
@@ -1,18 +1,24 @@
 package byu.codemonkeys.tickettoride.shared.results;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import byu.codemonkeys.tickettoride.shared.model.cards.DestinationCard;
 
-/**
- * Created by meganrich on 10/18/17.
- */
-
 public class DestinationCardResult extends Result {
+
     private List<DestinationCard> cards;
+
     public DestinationCardResult(List<DestinationCard> cards) {
         super();
-        for(DestinationCard card: cards)this.cards.add(card);
+        this.cards = cards;
+    }
+
+    public DestinationCardResult(Set<DestinationCard> cards) {
+        super();
+        this.cards = new ArrayList<>();
+        this.cards.addAll(cards);
     }
 
     public DestinationCardResult(String errorMessage) {
